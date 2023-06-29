3094adbcda3cbb7498095eb261b80f3d1265484a
mgstingl <matiasgstingl@gmail.com>
2023-06-29 16:54:07 +0200

    Running with cmd

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from imswitch.__main__ import main
+import sys
+from pathlib import Path
 
+import os
+
+locai_pth = str(Path(__file__).parent.parent.absolute())
+print("Locai path is ", locai_pth)
+sys.path.append(locai_pth)
+print(sys.path)
+from imswitch.__main__ import main
 main()
