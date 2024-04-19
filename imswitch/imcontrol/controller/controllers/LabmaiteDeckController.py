09b0b10b98da16fe08f8f4e3d6edca856738338b
matias <matiasgstingl@gmail.com>
2024-04-19 14:47:02 +0200

    Fixed parking position and door mechanism.

--- imswitch/imcontrol/controller/controllers/LabmaiteDeckController.py
+++ imswitch/imcontrol/controller/controllers/LabmaiteDeckController.py
@@ -52,6 +52,10 @@ if os.environ["APP"] == "BCALL":
     exp_name = r"bcall_bcells_concentrations_medium_static.json" # r"bcall_K562_test.json"
     exp_name = r"bcall_experiment_config_TEST.json" # r"bcall_K562_test.json"
     exp_name = r"Test96WellPlate1.json"
+    exp_name = r"TestGrid1.json"
+    exp_name = (r"BCALLJurkatTestShort.json")
+
+
 
     EXPERIMENT_JSON_PATH = os.sep.join([PROJECT_FOLDER, "config", exp_name])
 
@@ -360,9 +364,12 @@ class LabmaiteDeckController(LiveUpdatedController):
                 row.position_in_well_index = len(count_dict[key]) - 1
         else:
             key = (self.scan_list[row].slot, self.scan_list[row].well)
-            while key == (self.scan_list[row + 1].slot, self.scan_list[row + 1].well):
-                self.scan_list[row + 1].position_in_well_index += 1
-                row += 1
+            if len(self.scan_list) == 1:
+                self.scan_list[row].position_in_well_index += 1
+            else:
+                while key == (self.scan_list[row + 1].slot, self.scan_list[row + 1].well): # TODO: fix bug with single element list
+                    self.scan_list[row + 1].position_in_well_index += 1
+                    row += 1
 
     def update_list_in_widget(self):
         self._widget.update_scan_list(self.scan_list)
